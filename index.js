747fc8d6fdd06bb3bd55738523ddbb0adaab5cb6
emran <shuvoorba28@gmail.com>
2023-10-16 20:52:16 +0600

    users component created and attached with backend

--- index.js
+++ index.js
@@ -90,6 +90,21 @@ async function run() {
             res.send(result)
         })
 
+        // User related APIs
+
+        app.get('/user', async(req, res) => {
+            const cursor = userCollection.find(); 
+            const users = await cursor.toArray(); 
+            res.send(users); 
+        })
+
+        app.post('/user', async(req, res) => {
+            const user = req.body; 
+            console.log(user);
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
